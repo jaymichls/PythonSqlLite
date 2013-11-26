983e49d7372510069c92833b457e3ff42e0ad604
Jason <jaymichls@gmail.com>
2013-11-25 23:14:28 -0800

    Added basic wrapper functionality.

--- dbConnection.py
+++ dbConnection.py
@@ -1,38 +1,65 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 class Database:
 	import sqlite3 as lite
-	con = None 
+	
 	#TODO 
 	
 	def __init__(self, database):
 		self.database = database
-		connectToDB(self, self.database)
+		self.con = None
+		self.connectToDB()
 		
-	def connectToDB(self, database):
+	def connectToDB(self):
 		# Check to see if there is already an existing connection
+		
 		if self.con:
+			print 'Connection already exists'
 			return
 		
 		try:
-			self.con = lite.connect(database)
-		except lite.Error, e:
+			self.con = self.lite.connect(self.database)
+			print 'Connection established'
+		except self.lite.Error, e:
 			print "SQL Error %s:" % e.args[0]
 			
 	def closeDB(self):
 		if self.con:
-			con.close()
+			self.con.close()
+			print 'Connection Closed'
 	
 	def createSchema(self, schema): 
 	#TODO figure out how to store the schema
 		return
 	
-	def createTable(self, tableName, listOfColsAndTypes, colsAndTypes):
+	def createTable(self, tableName, listOfColsAndTypes):
 		# TODO: Create a table given the table name and the columns and column types.
 		# maybe pass colname type as a string need to be separated by ','
 		if self.con:
 			with self.con:
 				cur = self.con.cursor()
-				cur.execute("CREATE TABLE %s(%s)"%s(tableName, colsAndTypes))
+				print listOfColsAndTypes				
+				cur.execute("CREATE TABLE %s(test text)"%(tableName))
+	
+	def dropTable(self, tableName):
+		if self.con:
+			with self.con:
+				cur = self.con.cursor()
+				cur.execute("DROP TABLE %s"%(tableName))
+
+	def selectFrom(self, tableName): 
+		#TODO look into row factory and text factory 
+		if self.con:
+			with self.con:
+				cur = self.con.cursor()
+				for row in cur.execute("SELECT * FROM %s"%(tableName)):
+					print row
+								
+	def insertInto(self, tableName, value):
+		if self.con:
+			with self.con:
+				cur = self.con.cursor()
+				print value
+				cur.execute("INSERT INTO %s VALUES('sweet')"%(tableName))
+				self.con.commit()
