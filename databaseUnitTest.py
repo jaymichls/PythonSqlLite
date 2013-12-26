e7e585db9115c3496d9fb21c3482cfbce1c90fed
Jay <jay@laptop.(none)>
2013-12-26 01:09:03 -0800

    unit test stuff

--- databaseUnitTest.py
+++ databaseUnitTest.py
@@ -7,14 +7,21 @@ import unittest
 class TestTableFunctions(unittest.TestCase):
 
 	def setUp(self):
-		self.db = dbcon.Database('Test')
 		self.tableOne = dict(ColumnOne='TEXT',ColumnTwo='INTEGER')
 		self.tableName = 'TestTable'
-
-	def test_create(self):
+	
+	def test_connection(self):
+		self.db = dbcon.Database('Test')
+		self.assertTrue(self.db.database, 'Test')
+		
+	def test_create_table(self):
 		self.db.createTable(self.tableName, self.tableOne, [])
 		self.cur = dbcon.Database.getCursor(self.db)
 		self.assertTrue(dbcon.Database.tableExists(self.cur, self.tableName))
+
+	#def test_tables(self):
+
+	#def test_delete_table
 
 if __name__ == '__main__':
     unittest.main()
